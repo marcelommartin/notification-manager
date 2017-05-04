599e7cc5afbe793d99806374418363f5f8c9dcf0
Marcelo Marcatto Martin <marcelo.martin@inatel.br>
2017-05-04 15:19:29 -0300

    Initial firebase set up

--- notification-manager-ui/src/App.vue
+++ notification-manager-ui/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+ 
   </div>
 </template>
 
@@ -9,6 +8,21 @@
 export default {
   name: 'app'
 }
+
+import Firebase from 'firebase'
+
+let config = {
+    apiKey: "AIzaSyAmkmjZgfQtn5ZzzvCJ02OZkvI9Bln3mvo",
+    authDomain: "notification-manager-a274c.firebaseapp.com",
+    databaseURL: "https://notification-manager-a274c.firebaseio.com",
+    projectId: "notification-manager-a274c",
+    storageBucket: "notification-manager-a274c.appspot.com",
+    messagingSenderId: "585354264066"
+  };
+  
+let app = Firebase.initializeApp(config)
+let db = app.database()
+
 </script>
 
 <style>
---
+++ notification-manager-ui/src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login">          
+      <!-- Google sign-in holder -->
+      <!--<div class="g-signin2" data-onsuccess="onSignIn"></div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  methods: {    
+    onSignIn: function (googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      var id_token = googleUser.getAuthResponse().id_token;
+      console.log("ID Token: " + id_token);
+    }    
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
